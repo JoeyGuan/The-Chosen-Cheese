2cc094eb3378aa6c9e1133326e7a5bf6e5696f19
serp <marcokhl2005@gmail.com>
2023-01-11 14:35:39 -0500

    Added main enemy class
    Implemented the hp bar, split the enemy class into melee/ranged.

--- FinalProject/Enemies.java
+++ FinalProject/Enemies.java
@@ -6,14 +6,40 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Enemies extends Actor
+public abstract class Enemies extends Actor
 {
-    /**
-     * Act - do whatever the Enemies wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        // Add your action code here.
+    // Bar settings
+    private int hpBarHeight = 10;
+    private Color fillColor = new Color(145, 10, 10), barColor = Color.MAGENTA;
+    
+    // Stats - will need to implement a stat scaling system based on the 'level'
+    protected int level, hp, spd, atkDmg, def;
+    protected SuperStatBar hpBar;
+    
+    public Enemies(int hp, int spd){
+        this.hp = hp;
+        this.spd = spd;
+        
+        hpBar = new SuperStatBar(hp, hp, this, getImage().getWidth(), hpBarHeight, - getImage().getHeight() / 2 - hpBarHeight, fillColor, barColor, false, barColor, 3);
+    }
+    
+    public void addedToWorld(World w){
+        w.addObject(hpBar, getX(), getY());
+    }
+    
+    public void takeDamage(int dmg){
+        if(hp - dmg > 0){
+            hp -= dmg;
+            hpBar.update(hp);
+        }
+        else{
+            hp = 0;
+            hpBar.update(hp);
+            death();
+        }
+    }
+    
+    protected void death(){
+        getWorld().removeObject(this);
     }
 }
---
+++ FinalProject/RangedEnemy.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Ranged here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RangedEnemy extends Enemies
+{
+    public RangedEnemy(int hp, int spd){
+        super(hp, spd);
+    }
+    
+    /**
+     * Act - do whatever the Ranged wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
