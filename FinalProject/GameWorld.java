32f5864b5133041ba56d9c3c9ba3e1aeeea8d55f
ayujin <84431267+ayujin@users.noreply.github.com>
2023-01-24 14:51:37 -0500

    SOUND ADDED + Skipping Story Screen

--- FinalProject/GameWorld.java
+++ FinalProject/GameWorld.java
@@ -201,7 +201,7 @@ public class GameWorld extends World
         //Clear Screen
         removeObjects(getObjects(Actor.class));
         //Adding UI elements
-        addObject(new AttackTypeIndicator(), 50, 50);
+        addObject(new AttackTypeIndicator(), 100, 100);
         
         //Adds room layout
         int roomType = dungeonFloor[currentRoomY][currentRoomX];
