f5f5f73b5cfe8e89b3808293291e7dbe2d6c5b02
serp <marcokhl2005@gmail.com>
2023-01-23 22:51:06 -0500

    Minor caps error

--- FinalProject/StartScreen.java
+++ FinalProject/StartScreen.java
@@ -17,7 +17,7 @@ public class StartScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1300, 700, 1); 
-        setBackground(new GreenfootImage("startScreen.png"));
+        setBackground(new GreenfootImage("StartScreen.png"));
         
         GreenfootImage background = new GreenfootImage("startButton.png");
         background.scale(background.getWidth()/8, background.getHeight()/8);
